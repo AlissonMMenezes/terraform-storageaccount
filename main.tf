238d5bc3486e815ef9eefb5a4c841159a2e92048
Alisson Menezes <Alisson.itops@gmail.com>
2022-03-05 15:55:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,16 @@ resource "azurerm_storage_account" "sa" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+
+  blob_properties {
+    delete_retention_days = 7
+    container_delete_retention_policy = 7
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  
 }
 
 resource "azurerm_storage_container" "container" {
@@ -11,4 +21,15 @@ resource "azurerm_storage_container" "container" {
   name                  = each.key
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
+  is_hns_enabled        = "true"
+
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "datalake" {
+  name               = "example"
+  storage_account_id = azurerm_storage_account.sa.id
+
+  properties = {
+    hello = "aGVsbG8="
+  }
 }
