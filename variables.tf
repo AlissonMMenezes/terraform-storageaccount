bfa5e5f79e5b6044eb98230981162c89da171707
Alisson Menezes <Alisson.itops@gmail.com>
2022-03-05 15:25:30 +0100

    pushing first version of the module

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "storage_account_name" {
+  type        = string
+  description = "Storage Account Name"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group Name"
+}
+
+variable "location" {
+  type        = string
+  description = "Resource Group Name"
+  default     = "West Europe"
+}
